f3e6edfd5188c770ed555a501af1f61a25323940
Gabe Bravo <gabecbravo@gmail.com>
2017-08-16 18:26:39 -0400

    Added an env var for react.

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 
 // this is will add the client in deployment
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static('../client/build'));
 }
 
 // routes specific to driver contact CRUD
